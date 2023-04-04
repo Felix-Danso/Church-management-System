a524e5f3350ecadcff566b7f7294d74f006488a9
Felix-Danso <117346298+Felix-Danso@users.noreply.github.com>
2023-04-04 10:05:31 +0000

    update

--- church-management-system/src/Components/AuthenticationLayout/index.jsx
+++ church-management-system/src/Components/AuthenticationLayout/index.jsx
@@ -4,7 +4,7 @@ const AuthenticationLayout = (props) => {
   return (
     <div className='p-0 m-0 bg-center bg-gradient-to-tr from-[#00d5ff] via-[#3e3e45] to-[#df4ecb] ... table bg-no-repeat h-screen w-full'>
      <div className="form bg-[white] absolute max-md:left-[50%] max-md:-translate-x-1/2 md:left-[40%] lg:left-[50%] xl:left-[35%] 2xl:left-[40%] top-1/2 -translate-y-1/2 gap-5 grid">
-        <div className="flex flex-col h-96 w-96 items-center">
+        <div className="flex flex-col items-center h-96 w-96">
             {props.form}
          </div>  
      </div>
