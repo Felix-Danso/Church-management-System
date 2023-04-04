2019a27ca7ae2129a9d1b29136c92c8f0ed270a2
Felix-Danso <117346298+Felix-Danso@users.noreply.github.com>
2023-04-04 11:33:50 +0000

    Add

--- church-management-system/src/Components/AuthenticationLayout/index.jsx
+++ church-management-system/src/Components/AuthenticationLayout/index.jsx
@@ -3,11 +3,9 @@ import React from 'react'
 const AuthenticationLayout = (props) => {
   return (
     <div className='p-0 m-0 bg-center bg-gradient-to-tr from-[#00d5ff] via-[#3e3e45] to-[#df4ecb] ... table bg-no-repeat h-screen w-full'>
-     <div className="form bg-[white] absolute max-md:left-[50%] max-md:-translate-x-1/2 md:left-[40%] lg:left-[50%] xl:left-[35%] 2xl:left-[40%] top-1/2 -translate-y-1/2 gap-5 grid">
-        <div className="flex flex-col items-center h-96 w-96">
+        <div className="flex flex-col items-center justify-center float-right w-1/2 h-full h-96 bg-slate-200">
             {props.form}
          </div>  
-     </div>
     </div>
   )
 }
--- church-management-system/src/Components/LoginForms/index.jsx
+++ church-management-system/src/Components/LoginForms/index.jsx
@@ -7,8 +7,8 @@ const LoginForm = () => {
     const[userName,setUserName] = useState()
     const[email,setEmail] = useState()
   return (
-    <form className='flex flex-col h-72 gap-5 border '>
-        <h1 className='text-2xl flex justify-center font-bold'>
+    <form className='flex flex-col items-center justify-center w-3/4 gap-5 border bg-slate-600 h-96'>
+        <h1 className='flex justify-center text-2xl font-bold'>
             Login</h1>
         <Input label="name" type="text" placeholder="Enter your username" value={userName} handleChange={(event)=>setUserName(event.target.value)}/>
         <Input label="email" type="text" placeholder="Enter your email" value={email} handleChange={(event)=>setEmail(event.target.value)}/>
