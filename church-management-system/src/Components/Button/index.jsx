7d7b4b2a03ef3c903e1f5ef6cf2cea66ea1da471
Felix-Danso <117346298+Felix-Danso@users.noreply.github.com>
2023-04-13 14:15:34 +0000

    add

--- church-management-system/src/Components/Button/index.jsx
+++ church-management-system/src/Components/Button/index.jsx
@@ -4,7 +4,7 @@ const Button = (props) => {
   return (
     <button 
     type={props.type}
-    className={`w-[${props.width}] hover:from-gray_800 hover:to-gray_800 text-[white] bg-gradient-to-r from-primary to-secondary font-medium max-sm:font-normal rounded-lg text-base max-sm:px-1 px-5 py-2.5 text-center mr-3 md:mr-0`} 
+    className={`w-[${props.width}] bg-slate-500 hover:from-slate-500 hover:to-black text-[black] bg-gradient-to-r from-primary to-secondary font-medium max-sm:font-normal rounded-lg text-base max-sm:px-1 px-5 py-2.5 text-center mr-3 md:mr-0`} 
     onClick={props.onClick}
 >
     {props.name}
